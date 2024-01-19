0f7df1056de8ee25a75c69f5453bf719e8a20e0b
Pulkit1903 <500095602@stu.upes.ac.in>
2024-01-19 23:11:14 +0530

    Code Added

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static double calculate(double num1, double num2, char operator) {
+        switch (operator) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        try {
+            double result = calculate(num1, num2, operator);
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: Invalid operator");
+        }
+    }
+}
